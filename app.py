dc5a2c4a6ea5f11ffd4528a2ca449c00f00f9edb
DESKTOP-8QI5SLQ\Azhar Yoosuf <yoosuf.20210973@iit.ac.lk>
2025-03-17 20:29:34 +0530

    edited in app

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    port = int(os.environ.get('PORT', 5002))  # Use PORT from environment, default to 5002
+    app.run(host='0.0.0.0', port=port, debug=True)
